62d60644251087069f8feaa511c380889fa0a204
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 17:49:07 +0800

    Menambah router operator

--- api/v1/routes/operator.js
+++ api/v1/routes/operator.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const { authenticate } = require('../controllers/authenticate')
-const router = express.Router();
+const express = require('express')
+// const { authenticate } = require('../controllers/authenticate')
+const router = express.Router()
 
-router.get('/login', authenticate);
+router.get('/semester')
+router.post('/semester')
+router.put('/semester')
+router.delete('/semester')
 
-module.exports = router;
+router.patch('/status/pendaftaran')
+router.patch('/status/asdos')
+router.patch('/status/dosen')
+
+router.get('/jadwal')
+router.post('/jadwal')
+
+router.get('/jadwal/download')
+
+module.exports = router
