2b87db1402f18dbf223fa74bd4cc54f86aafae5c
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 14:47:20 +0800

    Menambah router auth

--- api/v1/routes/authenticate.js
+++ api/v1/routes/authenticate.js
@@ -3,5 +3,7 @@ const { authenticate } = require('../controllers/authenticate')
 const router = express.Router();
 
 router.get('/login', authenticate);
+router.get('/logout', authenticate);
+router.get('/authecticate', authenticate);
 
 module.exports = router;
