1057e055dbc526e8556590bb85561610e7e0d558
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 14:54:09 +0800

    Menambah router dosen

--- api/v1/routes/dosen.js
+++ api/v1/routes/dosen.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require('express')
 const { authenticate } = require('../controllers/authenticate')
-const router = express.Router();
+const router = express.Router()
 
-router.get('/login', authenticate);
+router.get('/login', authenticate)
 
-module.exports = router;
+router.get('/pendaftaran')
+router.get('/pendaftaran/download')
+router.put('/pendaftaran/terima')
+router.put('/pendaftaran/tolak')
+
+router.put('/booking/tambah/asdos')
+router.put('/booking/batal/asdos')
+
+module.exports = router
