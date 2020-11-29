d0b6d147e437d7a5f0e292ebc4a5d83f55ee5cc9
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 21:21:24 +0800

    Nothing Special

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { authenticate } = require('../controllers/admin')
+// const { authenticate } = require('../controllers/admin')
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', authenticate);
+router.get('/');
 
 module.exports = router;
