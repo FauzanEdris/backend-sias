ec9e22322c511afdfe04303ae735c8713e9cf701
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 21:19:59 +0800

    Menambah View User

--- api/v1/controllers/admin.js
+++ api/v1/controllers/admin.js
@@ -1,22 +1,22 @@
 const adminService = require('../services/admin')
 
-const { createBlogpost } = adminService
+const { view_users } = adminService
 
 module.exports = {
-  authenticate: async (req, res, next) => {
-    // const {user, content} = req.body
-    try {
-      const data = await createBlogpost()
-      // other service call (or same service, different function can go here)
-      // i.e. - await generateBlogpostPreview()
-      // console.log('woi')
-      res.json(data)
-      next()
-    } catch(e) {
-      console.log(e.message)
-      // res.sendStatus(500) && next(error)
-    }
-  },
+    view_users: async (req, res, next) => {
+      const { skip, limit } = req.query
+      try {
+        const data = await view_users({ skip, limit })
+        // other service call (or same service, different function can go here)
+        // i.e. - await generateBlogpostPreview()
+  
+        res.json(data)
+        next()
+      } catch(e) {
+        console.log(e.message)
+        // res.sendStatus(500) && next(error)
+      }
+    },
   add_users: async (req, res, next) => {
     const { data } = req.body
     try {
--- api/v1/services/admin.js
+++ api/v1/services/admin.js
@@ -1,17 +1,17 @@
-const { blogpostDb } = require('../models/users')
+const { view_all } = require('../models/users')
 
 /*
   * if you need to make calls to additional tables, data stores (Redis, for example), 
   * or call an external endpoint as part of creating the blogpost, add them to this service
 */
-const createBlogpost = async (user, content) => {
-  try {
-    return await blogpostDb(user, content)
-  } catch(e) {
-    throw new Error(e.message)
-  }
-}
 
 module.exports = {
-  createBlogpost
+  view_users: async ({ skip, limit }) => {
+    try {
+      const projection = { name: 1, email: 1, status: 1 }
+      return await view_all({ projection, skip, limit })
+    } catch(e) {
+      throw new Error(e.message)
+    }
+  }
 }
