4f2f5e548316eb0033ed86a5dc9800976232a535
FauzanEdris <fauzanedris@yahoo.com>
2020-11-29 18:05:38 +0800

    Menambahkan router Akademik

--- api/v1/routes/akademik.js
+++ api/v1/routes/akademik.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { authenticate } = require('../controllers/authenticate')
+// const { authenticate } = require('../controllers/authenticate')
 const router = express.Router();
 
-router.get('/login', authenticate);
+router.get('/asdos')
+
+router.get('/kegiatan')
+router.put('/kegiatan/terima')
+router.put('/kegiatan/tolak')
 
 module.exports = router;
